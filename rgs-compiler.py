0ca13a840e5060d0a7b61d4b90b01c41a0ccfa3c
Johnny Stene <jhonnystene@gmail.com>
2020-06-07 10:14:17 -0600

    More code documentation and error checking

--- rgs-compiler.py
+++ rgs-compiler.py
@@ -43,26 +43,38 @@ def convertLine(line):
 		
 
 print("ReallyGoodScript Compiler")
+
+# First, we need to make sure we have all of the arguments we need.
 try:
 	filename = sys.argv[1]
 	outfile = sys.argv[2]
 except:
 	print("USAGE: " + sys.argv[0] + " <input filename> <output filename>")
 	exit()
-	
-with open(filename, 'r') as infile:
-	fileContents = infile.read()
 
-if("\r\n" in fileContents):
+# We do. Load in the input file:
+print("Loading input file...")
+try:
+	with open(filename, 'r') as infile:
+		fileContents = infile.read()
+except:
+	print("ERROR! Couldn't read input file (" + filename + ")")
+	exit()
+
+# Detect what kind of newlines we have.
+print("Detecting newlines...")
+if("\r\n" in fileContents): # With carriage return?
 	print("Detected Windows newlines.")
 	fileContents = fileContents.split("\r\n")
-else:
+else: # Or without carriage return?
 	print("Detected *nix newlines.")
 	fileContents = fileContents.split("\n")
 
-assembly = "BITS 16\n"
+# Basic header
+assembly = "BITS 16\ndisk_buffer equ 24576\n"
 
 # We need to include the call vectors or applications won't be able to interface w/ the OS
+print("Loading call vectors...")
 try:
 	with open("rgs/callvectors.asm", 'r') as vectorFile: # TODO: Make callvectors.asm an optional argument
 		assembly += vectorFile.read() + "\n"
@@ -70,17 +82,21 @@ except:
 	print("ERROR! Couldn't read call vectors (rgs/callvectors.asm)")
 	exit()
 
-assembly += "main:\n" # Boot process (stack setup, segmenting, etc...) is handled by rgs_boot so we go there first
+# Main function. This gets called by rgs_boot after everything has been set up.
+assembly += "main:\n"
 
-# Compile to assembly
+# Run through each line and convert it to assembly
+print("Compiling to Assembly...")
 for line in fileContents:
 	assembly += convertLine(line)
 
-# Add all of the strings
+# Add all of the strings to the ASM file
+print("Adding strings...")
 for i in range(len(strings)):
 	assembly += "string" + str(i) + " db " + strings[i] + ", 13, 10, 0\n"
 	
-# Finally, include the rest of the ASM needed for the OS
+# Finally, include all of the code for the rest of the functions
+print("Importing functions...")
 try:
 	with open("rgs/functions.asm", 'r') as functionfile: # TODO: Make functions.asm an optional argument
 		assembly += functionfile.read() + "\n"
@@ -88,9 +104,13 @@ except:
 	print("ERROR! Couldn't read function include file (rgs/functions.asm)")
 	exit()
 
+# Write out all of the code to assembly.
+print("Saving...")
 try:
 	with open(outfile, 'w') as output:
 		output.write(assembly)
 except:
 	print("ERROR! Couldn't write to output file (outfile)")
 	exit()
+
+print("Success! Outputted assembly code to " + outfile + ".")
