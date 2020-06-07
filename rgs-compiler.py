f3792ba8403b6bc2787b2c411fe3464fcbe19040
Johnny Stene <jhonnystene@gmail.com>
2020-06-07 10:01:47 -0600

    Add loading in of call vectors + function code

--- rgs-compiler.py
+++ rgs-compiler.py
@@ -60,7 +60,17 @@ else:
 	print("Detected *nix newlines.")
 	fileContents = fileContents.split("\n")
 
-assembly = "BITS 16\njmp rgs_boot\nmain:\n" # Boot process (stack setup, segmenting, etc...) is handled by rgs_boot so we go there first
+assembly = "BITS 16\n"
+
+# We need to include the call vectors or applications won't be able to interface w/ the OS
+try:
+	with open("rgs/callvectors.asm", 'r') as vectorFile: # TODO: Make callvectors.asm an optional argument
+		assembly += vectorFile.read() + "\n"
+except:
+	print("ERROR! Couldn't read call vectors (rgs/callvectors.asm)")
+	exit()
+
+assembly += "main:\n" # Boot process (stack setup, segmenting, etc...) is handled by rgs_boot so we go there first
 
 # Compile to assembly
 for line in fileContents:
@@ -69,6 +79,18 @@ for line in fileContents:
 # Add all of the strings
 for i in range(len(strings)):
 	assembly += "string" + str(i) + " db " + strings[i] + ", 13, 10, 0\n"
+	
+# Finally, include the rest of the ASM needed for the OS
+try:
+	with open("rgs/functions.asm", 'r') as functionfile: # TODO: Make functions.asm an optional argument
+		assembly += functionfile.read() + "\n"
+except:
+	print("ERROR! Couldn't read function include file (rgs/functions.asm)")
+	exit()
 
-with open(outfile, 'w') as output:
-	output.write(assembly)
+try:
+	with open(outfile, 'w') as output:
+		output.write(assembly)
+except:
+	print("ERROR! Couldn't write to output file (outfile)")
+	exit()
